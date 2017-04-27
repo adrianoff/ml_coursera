67a66a53661f1583ec005d18afa1514b03d70a7c
adrianoff <adrianov87@gmail.com>
2017-04-27 18:43:24 +0300

    c2w4

---
+++ curs2/week4/bagging_and_random_forest.py
@@ -0,0 +1,26 @@
+from sklearn import datasets, tree
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import BaggingClassifier
+import math
+
+digits = datasets.load_digits()
+
+X = digits['data']
+y = digits['target']
+
+clf = tree.DecisionTreeClassifier()
+scores = cross_val_score(clf, X, y, cv=10)
+print scores.mean()
+
+clf_Bagging = BaggingClassifier(clf, n_estimators=100)
+scores = cross_val_score(clf_Bagging, X, y, cv=10)
+print scores.mean()
+
+clf_Bagging_d = BaggingClassifier(clf, n_estimators=100, max_features=8)
+scores = cross_val_score(clf_Bagging_d, X, y, cv=10)
+print scores.mean()
+
+clf = tree.DecisionTreeClassifier(max_features=8)
+clf_Bagging_d2 = BaggingClassifier(clf, n_estimators=100)
+scores = cross_val_score(clf_Bagging_d2, X, y, cv=10)
+print scores.mean()
