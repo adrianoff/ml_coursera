21ccd38c7ee745337071d28f74cd5df8159ec695
adrianoff <adrianov87@gmail.com>
2017-07-12 19:29:19 +0300

    c4w1

--- curs4/week1/test4.py
+++ curs4/week1/test4.py
@@ -1,36 +1,14 @@
 from scipy import stats
+import pandas as pd
+import numpy as np
+
 print stats.binom_test(67, 100, 0.75)
 
-import pandas as pd
 df = pd.read_table('pines.txt')
 print df.describe()
 
-step = 40
-cluster_coords = []
-for sn in range(0, 200, step):
-    for we in range(0, 200, step):
-        cluster_coords.append([sn, we])
+tree_cnt = stats.binned_statistic_2d(df['sn'], df['we'], np.ones(df.shape[0]), statistic='count', bins=5)
+observed_freq = np.reshape(tree_cnt.statistic, (25, 1))
+print sum(observed_freq)
 
-print cluster_coords
-
-pines = []
-first = True
-for cluster in cluster_coords:
-    if first:
-        new_pines_cluster = df[
-            (df.sn >= cluster[0]) &
-            (df.we >= cluster[1]) &
-            (df.sn < cluster[0]+step) &
-            (df.we < cluster[1]+step)
-        ]
-    else:
-        new_pines_cluster = df[
-            (df.sn > cluster[0]) &
-            (df.we > cluster[1]) &
-            (df.sn <= cluster[0]+step) &
-            (df.we <= cluster[1]+step)
-        ]
-    pines.append(len(new_pines_cluster))
-    first = False
-
-print sum(pines)
+print stats.chisquare(observed_freq)
