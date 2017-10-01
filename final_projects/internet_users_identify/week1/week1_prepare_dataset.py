adb0cadd2eb67dbbc91fc5feec0df5dce8f30725
adrianoff <adrianov87@gmail.com>
2017-10-02 02:03:00 +0300

    fpw1

--- final_projects/internet_users_identify/week1/week1_prepare_dataset.py
+++ final_projects/internet_users_identify/week1/week1_prepare_dataset.py
@@ -9,7 +9,7 @@ from tqdm import tqdm_notebook
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
-import glob
+import glob, re, ntpath
 
 PATH_TO_DATA = ('../capstone_user_identification/')
 
@@ -20,6 +20,44 @@ def prepare_train_set(path_to_csv_files, session_length=10):
     for file in files:
         sites = pd.concat([sites, pd.read_csv(file)])
 
-    print(sites.groupby('site').head(100))
+    sites['count'] = 0
+    sites_count = sites.groupby('site')['count'].agg('count')
 
-prepare_train_set(os.path.join(PATH_TO_DATA, '3users/'), session_length=10)
+    sites = pd.DataFrame(columns=['site', 'total_visits'])
+    sites['site'] = sites_count.index
+    sites['total_visits'] = sites_count.values
+    sites.sort_values(['total_visits'], inplace=True, ascending=False)
+    sites = sites.reset_index(drop=True)
+    sites.index = sites.index + 1
+
+    columns = sites.site.values
+    columns = np.append(columns, 'user_id')
+    train_data = pd.DataFrame(columns=columns)
+
+    for file in files:
+        df = pd.read_csv(file)
+        user_id = int(re.findall(r'\d+', ntpath.basename(file))[0])
+
+        k = 0
+        s = 1
+        cur_index = len(train_data.index) - 1
+        for site in df.site.values:
+            new_index = cur_index + k
+
+            if new_index not in train_data.index:
+                train_data.loc[new_index] = 0
+                train_data.loc[new_index]['user_id'] = user_id
+
+            train_data.loc[new_index][site] = train_data.loc[new_index][site] + 1
+            s = s + 1
+            if s > session_length:
+                k = k + 1
+                s = 1
+
+    return train_data
+
+
+train_data = prepare_train_set(os.path.join(PATH_TO_DATA, '3users/'), session_length=10)
+train_data
+
+#prepare_train_set(os.path.join(PATH_TO_DATA, '3users/'), session_length=10)
