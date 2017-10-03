8aea226551e93182525b12163c986d233a60e203
adrianoff <adrianov87@gmail.com>
2017-10-04 00:58:57 +0300

    fpw1

--- final_projects/internet_users_identify/week1/week1_prepare_dataset.py
+++ final_projects/internet_users_identify/week1/week1_prepare_dataset.py
@@ -10,9 +10,9 @@ import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
 import glob, re, ntpath
+import sys
 
 PATH_TO_DATA = ('../capstone_user_identification/')
-
 
 def prepare_train_set(path_to_csv_files, session_length=10):
     files = glob.glob(path_to_csv_files + 'user*.csv')
@@ -23,41 +23,57 @@ def prepare_train_set(path_to_csv_files, session_length=10):
     sites['count'] = 0
     sites_count = sites.groupby('site')['count'].agg('count')
 
-    sites = pd.DataFrame(columns=['site', 'total_visits'])
-    sites['site'] = sites_count.index
+    sites = pd.DataFrame(columns=['site_id', 'total_visits'])
     sites['total_visits'] = sites_count.values
-    sites.sort_values(['total_visits'], inplace=True, ascending=False)
-    sites = sites.reset_index(drop=True)
-    sites.index = sites.index + 1
+    sites.index = sites_count.index
+    sites.sort_values(['total_visits'], inplace=True, ascending=False, kind='heapsort')
+    sites['site_id'] = [j for j in range(1, len(sites.index)+1)]
 
-    columns = sites.site.values
-    columns = np.append(columns, 'user_id')
-    train_data = pd.DataFrame(columns=columns)
+    columns = ['site' + str(i) for i in range(1, session_length + 1)]
+    columns.append('user_id')
 
+    train_data = pd.DataFrame(columns=columns, dtype='int')
+    train_data_arr = {}
+
+    l = 0
     for file in files:
+        l = l+1
+
+        sys.stdout.write(file + " " + str(l) + "\n")
+        sys.stdout.flush()
+
         df = pd.read_csv(file)
         user_id = int(re.findall(r'\d+', ntpath.basename(file))[0])
 
         k = 0
         s = 1
-        cur_index = len(train_data.index) - 1
+        cur_index = len(train_data_arr)
+
         for site in df.site.values:
             new_index = cur_index + k
 
-            if new_index not in train_data.index:
-                train_data.loc[new_index] = 0
-                train_data.loc[new_index]['user_id'] = user_id
+            if new_index not in train_data_arr.keys():
+                train_data_arr[new_index] = {}
+                train_data_arr[new_index]['user_id'] = user_id
 
-            train_data.loc[new_index][site] = train_data.loc[new_index][site] + 1
+            train_data_arr[new_index]['site' + str(s)] = int(sites.loc[site]['site_id'])
+
             s = s + 1
             if s > session_length:
                 k = k + 1
                 s = 1
 
-    return train_data
+    train_data = train_data.from_dict(train_data_arr, dtype=int)
+    train_data.fillna(0, inplace=True)
+    train_data = train_data.transpose()
+    train_data = train_data[columns]
+    train_data = train_data.astype(np.int)
 
+    return train_data, sites
 
-train_data = prepare_train_set(os.path.join(PATH_TO_DATA, '3users/'), session_length=10)
-train_data
+train_data, site_freq_users = prepare_train_set(os.path.join(PATH_TO_DATA, '150users/'), session_length=10)
+
+print (train_data.shape)
+print (site_freq_users.shape)
 
 #prepare_train_set(os.path.join(PATH_TO_DATA, '3users/'), session_length=10)
