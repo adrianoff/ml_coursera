ab7c96fc1e2bdddf06955e0ca739341db235bf29
adrianoff <adrianov87@gmail.com>
2017-10-04 20:36:16 +0300

    fpw1

--- final_projects/internet_users_identify/week1/week1_prepare_dataset.py
+++ final_projects/internet_users_identify/week1/week1_prepare_dataset.py
@@ -5,7 +5,9 @@ from glob import glob
 import os
 import pickle
 #pip install tqdm
-from tqdm import tqdm_notebook
+
+from tqdm import tqdm
+
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
@@ -14,11 +16,52 @@ import sys
 
 PATH_TO_DATA = ('../capstone_user_identification/')
 
+
+def prepare_train_set2(path_to_csv_files, session_length=10):
+    site_dict = {}
+    site_id = 1
+    user_id = 1
+    sessions = []
+    i = 0  # session row initialization
+
+    for file in tqdm(glob.glob(path_to_csv_files + '/*.csv')):
+        sessions.append([0] * session_length + [user_id])
+        df = pd.read_csv(file, usecols=[1], squeeze=True)
+        j = 0  # session column initialization
+
+        for k, site in enumerate(df):
+            # Fill site dictionary
+            if site in site_dict:
+                site_dict[site] = (site_dict[site][0], site_dict[site][1] + 1)
+            else:
+                site_dict[site] = (site_id, 1)
+                site_id += 1
+
+            # Fill sessions list
+            sessions[i][j] = site_dict[site][0]
+            j += 1
+            if k == (len(df) - 1):
+                i += 1
+                j = 0
+                user_id += 1
+                continue
+            if (j // session_length) == 1:
+                sessions.append([0] * session_length + [user_id])
+                i += 1
+                j = 0
+
+    # Convert sessions list to dataframe
+    columns = ['site1', 'site2', 'site3', 'site4', 'site5',
+               'site6', 'site7', 'site8', 'site9', 'site10', 'user_id']
+    sessions_df = pd.DataFrame(np.array(sessions), columns=columns)
+
+    return sessions_df, site_dict
+
 def prepare_train_set(path_to_csv_files, session_length=10):
     files = glob.glob(path_to_csv_files + 'user*.csv')
     sites = pd.DataFrame()
-    for file in files:
-        sites = pd.concat([sites, pd.read_csv(file)])
+    for session_file in files:
+        sites = pd.concat([sites, pd.read_csv(session_file)])
 
     sites['count'] = 0
     sites_count = sites.groupby('site')['count'].agg('count')
@@ -34,16 +77,19 @@ def prepare_train_set(path_to_csv_files, session_length=10):
 
     train_data = pd.DataFrame(columns=columns, dtype='int')
     train_data_arr = {}
+    train_data_arr_keys = []
+
+    sites_list = sites.to_dict()
 
     l = 0
-    for file in files:
-        l = l+1
+    for session_file in files:
+        l += 1
 
-        sys.stdout.write(file + " " + str(l) + "\n")
+        sys.stdout.write(session_file + " " + str(l) + "\n")
         sys.stdout.flush()
 
-        df = pd.read_csv(file)
-        user_id = int(re.findall(r'\d+', ntpath.basename(file))[0])
+        df = pd.read_csv(session_file)
+        user_id = int(re.findall(r"\d+", ntpath.basename(session_file))[0])
 
         k = 0
         s = 1
@@ -52,28 +98,30 @@ def prepare_train_set(path_to_csv_files, session_length=10):
         for site in df.site.values:
             new_index = cur_index + k
 
-            if new_index not in train_data_arr.keys():
-                train_data_arr[new_index] = {}
+            if new_index not in train_data_arr_keys:
+                train_data_arr_keys.append(new_index)
+
+                train_data_arr[new_index] = 0
                 train_data_arr[new_index]['user_id'] = user_id
 
-            train_data_arr[new_index]['site' + str(s)] = int(sites.loc[site]['site_id'])
+            train_data_arr[new_index]['site' + str(s)] = sites_list['site_id'][site]
 
-            s = s + 1
+            s += 1
             if s > session_length:
-                k = k + 1
+                k += 1
                 s = 1
 
     train_data = train_data.from_dict(train_data_arr, dtype=int)
-    train_data.fillna(0, inplace=True)
-    train_data = train_data.transpose()
-    train_data = train_data[columns]
-    train_data = train_data.astype(np.int)
+    # train_data.fillna(0, inplace=True)
+    # train_data = train_data.transpose()
+    # train_data = train_data[columns]
+    # train_data = train_data.astype(np.int)
 
     return train_data, sites
 
-train_data, site_freq_users = prepare_train_set(os.path.join(PATH_TO_DATA, '150users/'), session_length=10)
+train_data, site_freq_users = prepare_train_set2(os.path.join(PATH_TO_DATA, '150users/'), session_length=10)
 
 print (train_data.shape)
-print (site_freq_users.shape)
+#print (site_freq_users.shape)
 
 #prepare_train_set(os.path.join(PATH_TO_DATA, '3users/'), session_length=10)
