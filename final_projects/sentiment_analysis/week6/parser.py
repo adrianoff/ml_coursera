12473cf74aaf1a1517f8d73a07ac1c2f2db1893a
adrianoff <adrianov87@gmail.com>
2017-11-15 00:22:28 +0300

    fp w6

--- final_projects/sentiment_analysis/week6/parser.py
+++ final_projects/sentiment_analysis/week6/parser.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+
 import requests
 import bs4 as bs4
+from tqdm import tqdm
+import re
 
+exp = re.findall('[А-Я][^А-Я]*'.decode('utf-8'), 'Тест Вв фывфыв Ввйцу Еррр Строки asdasda asdasdasd ываываыва ываыаываыва'.decode('utf-8'))
+
+print "\n"
+
+site = 'https://technopoint.ru'
 base_url = 'https://technopoint.ru/catalog/17a75b8e16404e77/smartfony'
 pages = []
 
@@ -8,11 +17,45 @@ for p in range(1, 33):
     pages.append(base_url + '/?p=' + str(p) + '&i=1&stock=0&order=4')
 
 products_link = []
-for page in pages:
+for page in tqdm(pages):
     req = requests.get(page)
     parser = bs4.BeautifulSoup(req.text, 'lxml')
     links = parser.findAll('a', attrs={'data-role': 'product-cart-link'})
     for link in links:
-        products_link.append(base_url + link['href'] + 'opinion/')
+        products_link.append(site + link['href'] + 'opinion/')
+        break
+    break
 
-print products_link
+pages_with_comments = []
+for link in tqdm(products_link):
+    req = requests.get(link)
+    parser = bs4.BeautifulSoup(req.text, 'lxml')
+    ul_pagination = parser.find('ul', {"class": "pagination"})
+    comments_pager_last_link = None
+    if ul_pagination is not None:
+        comments_pager_last_link = ul_pagination.find('li', {"class": "last"}).find('a')
+
+    comments_pages_count = 1
+    if comments_pager_last_link is not None:
+        comments_pages_count = int(comments_pager_last_link["data-page"]) + 1
+
+    for page_with_comments in range(1, comments_pages_count + 1):
+        pages_with_comments.append(link + str(page_with_comments) + '/')
+
+i = 1
+for page_with_comments in tqdm(pages_with_comments):
+    req = requests.get(page_with_comments)
+    parser = bs4.BeautifulSoup(req.text, 'lxml')
+    opinion_items = parser.find('div', {"class": "opinion-container"}).findAll('div', {"class": "opinion-item"})
+    for opinion_item in opinion_items:
+        description = str(opinion_item.find('div', {"class": "descriptions"})).replace("\n", ' ')
+        grade = opinion_item["data-grade"]
+
+        comment_file = open('./data/' + str(i) + '.txt', 'w')
+        comment_file.write(grade + "\n" + description)
+        comment_file.close()
+
+        i += 1
+
+print "\n"
+print "Total comments: " + str(i)
